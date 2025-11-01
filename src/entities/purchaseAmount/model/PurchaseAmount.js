00c44562bcc80ec8146794870ee50b4c8595a1cb
TH_97 <hood3392@gmail.com>
2025-11-01 12:28:00 +0900

    feat(entities) : 구입 금액 모델 구현

---
+++ src/entities/purchaseAmount/model/PurchaseAmount.js
@@ -0,0 +1,17 @@
+export class PurchaseAmount {
+  #purchaseAmount;
+
+  constructor(purchaseAmount) {
+    this.#vaildate(purchaseAmount);
+    this.#purchaseAmount = Number(purchaseAmount);
+  }
+
+  #vaildate(purchaseAmount) {
+    if (Number(purchaseAmount) % 1000 !== 0)
+      throw new Error("[ERROR] 1000 단위 여야 합니다");
+  }
+
+  getPurchaseAmount() {
+    return this.#purchaseAmount;
+  }
+}
