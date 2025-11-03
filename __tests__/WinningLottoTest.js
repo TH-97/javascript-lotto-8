7ed6ac632db4d972f8bd8742b2a5c13fb17ae091
TH_97 <hood3392@gmail.com>
2025-11-03 17:15:17 +0900

    refactor(Test) : 당첨 번호 클래스 수정

--- __tests__/WinningLottoTest.js
+++ __tests__/WinningLottoTest.js
@@ -1,43 +1,23 @@
-import { WinningLotto } from "../src/entities/winningLotto/index.js";
+import { WinningLotto } from "../src/entities/lotto/index.js";
 
 describe("winnginLotto 클래스 테스트", () => {
-  test("잘못된 값 입력시 예외 발생", () => {
+  test("보너스 넘버와 로또 번호가 중복식 예외 발생", () => {
     expect(() => {
-      new WinningLotto("1, 2, 3, 4, 5, 6, 7", 1);
+      new WinningLotto([1, 2, 3, 4, 5, 6], 1);
     }).toThrow("[ERROR]");
   });
-  test("잘못된 값 입력시 예외 발생2", () => {
+  test("보너스 넘버 잘못된 입력시 예외 발생", () => {
     expect(() => {
-      new WinningLotto("1, 2, 3, 4, 5, 6", 46);
+      new WinningLotto([1, 2, 3, 4, 5, 6], 46);
     }).toThrow("[ERROR]");
   });
-  test("잘못된 값 입력시 예외 발생3", () => {
+  test("보너스 넘버 잘못된 입력시 예외 발생2", () => {
     expect(() => {
-      new WinningLotto("1, 2, 3, 4, 5, 46", 1);
-    }).toThrow("[ERROR]");
-  });
-  test("잘못된 값 입력시 예외 발생4", () => {
-    expect(() => {
-      new WinningLotto("1, 2, 3, 4, 5, 46", -1);
-    }).toThrow("[ERROR]");
-  });
-  test("잘못된 값 입력시 예외 발생5", () => {
-    expect(() => {
-      new WinningLotto("-1, 2, 3, 4, 5, 46", 1);
-    }).toThrow("[ERROR]");
-  });
-  test("잘못된 값 입력시 예외 발생6", () => {
-    expect(() => {
-      new WinningLotto("2, 3, 4, 5, 46", 1);
-    }).toThrow("[ERROR]");
-  });
-  test("잘못된 값 입력시 예외 발생7", () => {
-    expect(() => {
-      new WinningLotto("1, 2, 3, 4, 5, 45", 1);
+      new WinningLotto([1, 2, 3, 4, 5, 6], -1);
     }).toThrow("[ERROR]");
   });
   test("3개가 2개 match시", () => {
-    const winning = new WinningLotto("1, 2, 3, 4, 5, 6", 7);
+    const winning = new WinningLotto([1, 2, 3, 4, 5, 6], 7);
     const result = winning.getMatchNumbs([
       [1, 2, 3, 10, 11, 12],
       [1, 2, 7, 10, 11, 12],
@@ -52,7 +32,7 @@ describe("winnginLotto 클래스 테스트", () => {
     });
   });
   test("4개가 1개 match시", () => {
-    const winning = new WinningLotto("1, 2, 3, 4, 5, 6", 7);
+    const winning = new WinningLotto([1, 2, 3, 4, 5, 6], 7);
     const result = winning.getMatchNumbs([
       [1, 2, 3, 4, 11, 12],
       [1, 2, 4, 7, 11, 12],
@@ -67,7 +47,7 @@ describe("winnginLotto 클래스 테스트", () => {
     });
   });
   test("4개 + 보너스 1개 match시", () => {
-    const winning = new WinningLotto("1, 2, 3, 4, 5, 6", 7);
+    const winning = new WinningLotto([1, 2, 3, 4, 5, 6], 7);
     const result = winning.getMatchNumbs([[1, 2, 3, 4, 7, 12]]);
 
     expect(result).toEqual({
@@ -79,7 +59,7 @@ describe("winnginLotto 클래스 테스트", () => {
     });
   });
   test("5개 match시", () => {
-    const winning = new WinningLotto("1, 2, 3, 4, 5, 6", 7);
+    const winning = new WinningLotto([1, 2, 3, 4, 5, 6], 7);
     const result = winning.getMatchNumbs([[1, 2, 3, 4, 5, 12]]);
 
     expect(result).toEqual({
@@ -91,7 +71,7 @@ describe("winnginLotto 클래스 테스트", () => {
     });
   });
   test("6개 match시", () => {
-    const winning = new WinningLotto("1, 2, 3, 4, 5, 6", 7);
+    const winning = new WinningLotto([1, 2, 3, 4, 5, 6], 7);
     const result = winning.getMatchNumbs([
       [1, 2, 3, 4, 5, 6],
       [1, 2, 3, 4, 5, 7],
