495edc13894b2545ae56d92444cfe4f7f0cc78c6
TH_97 <hood3392@gmail.com>
2025-11-03 17:17:02 +0900

    refactor(Controller) : 기능에 따른 수정

--- src/page/lottoController.js
+++ src/page/lottoController.js
@@ -1,6 +1,8 @@
+import { Console } from "@woowacourse/mission-utils";
 import { PurchaseAmount } from "../entities/purchaseAmount/index.js";
 import { RandomLottoNumberArray } from "../entities/randomLottoNumberArray/index.js";
-import { WinningLotto } from "../entities/winningLotto/index.js";
+import { WinningLotto } from "../entities/lotto/index.js";
+import { calculateProfit } from "../features/calculate/index.js";
 import { inputValue } from "../features/input/index.js";
 import {
   outputPurchaseAmountMessage,
@@ -9,31 +11,46 @@ import {
   outputWinningNumberMessage,
   outputBounsNumberMessage,
   outputMatchMessage,
+  outputProfit,
 } from "../features/output/index.js";
+import { stringSplitter } from "../features/splitter/index.js";
+import Lotto from "../entities/lotto/lotto/model/Lotto.js";
 
 export default async function lottoController() {
-  outputPurchaseAmountMessage();
+  try {
+    outputPurchaseAmountMessage();
 
-  const input = await inputValue();
+    const inputPurchaseAmount = await inputValue();
 
-  const purchasedLottoCount = new PurchaseAmount(
-    input
-  ).getPurchasedLottoCount();
+    const purchasedLottoCount = new PurchaseAmount(
+      inputPurchaseAmount
+    ).getPurchasedLottoCount();
 
-  outputPurchaseCount(purchasedLottoCount);
+    outputPurchaseCount(purchasedLottoCount);
 
-  const randomLottoArray = new RandomLottoNumberArray(
-    purchasedLottoCount
-  ).getRandomLottoNumberArray();
+    const randomLottoArray = new RandomLottoNumberArray(
+      purchasedLottoCount
+    ).getRandomLottoNumberArray();
 
-  outputRandomLottoNumbers(randomLottoArray);
+    outputRandomLottoNumbers(randomLottoArray);
 
-  outputWinningNumberMessage();
-  const inputWinningNumber = await inputValue();
-  outputBounsNumberMessage();
-  const inputBounsNumber = await inputValue();
+    outputWinningNumberMessage();
+    const inputWinningNumber = await inputValue();
 
-  const winnginLotto = new WinningLotto(inputWinningNumber, inputBounsNumber);
-  const matchObject = winnginLotto.getMatchNumbs(randomLottoArray);
-  outputMatchMessage(matchObject);
+    outputBounsNumberMessage();
+    const inputBounsNumber = await inputValue();
+
+    const lottoArray = stringSplitter(inputWinningNumber);
+    const lotto = await new Lotto(lottoArray).getNumbers();
+
+    console.log(lotto);
+
+    const winnginLotto = new WinningLotto(lotto, inputBounsNumber);
+    const matchObject = winnginLotto.getMatchNumbs(randomLottoArray);
+    outputMatchMessage(matchObject);
+    const calculatedProfit = calculateProfit(matchObject, inputPurchaseAmount);
+    outputProfit(calculatedProfit);
+  } catch (error) {
+    Console.print(error.message);
+  }
 }
