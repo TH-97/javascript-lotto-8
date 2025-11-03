24fb62da05e0ff66b255905b53b33689a3eeee3c
TH_97 <hood3392@gmail.com>
2025-11-03 17:13:27 +0900

    feat(Test) : 로또 클래스 테스트 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,4 +1,4 @@
-import Lotto from "../src/Lotto";
+import Lotto from "../src/entities/lotto/lotto/model/Lotto";
 
 describe("로또 클래스 테스트", () => {
   test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
@@ -15,4 +15,16 @@ describe("로또 클래스 테스트", () => {
   });
 
   // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+
+  test("로또 번호에 숫자가 부족하면 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("로또 번호에 음수가 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, -6]);
+    }).toThrow("[ERROR]");
+  });
 });
